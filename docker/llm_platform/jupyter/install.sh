e46b6819801ac5d6bfd022e21ec82cc0baef4287
Linh Ngo <linh.b.ngo@gmail.com>
2025-07-17 09:07:54 -0400

    adding elements

--- docker/llm_platform/jupyter/install.sh
+++ docker/llm_platform/jupyter/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install --no-cache-dir jupyter ollama
+pip install --no-cache-dir jupyter ollama langchain langchain-commmunity
 
 sudo cp /build/entrypoint.sh /usr/local/bin/entrypoint.sh
 sudo chmod 755 /usr/local/bin/entrypoint.sh
