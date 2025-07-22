1e7adc8df59fd80138553deba66529c31bd6711d
Linh B Ngo <lngo@wcupa.edu>
2025-07-22 14:17:43 -0400

    Update install.sh

--- docker/csc467/master-instructor/install.sh
+++ docker/csc467/master-instructor/install.sh
@@ -10,6 +10,10 @@ log_info "Master image"
 # install code-serve
 sh /build/code-server.sh --prefix=/usr/local/
 
+apt update
+apt install -y unzip
+apt clean
+
 pip3 install jupyter-book mkdocs-material markdown-include mkdocs-table-reader-plugin mkdocs-jupyter mkdocs-glightbox
 
 cp /build/entrypoint.sh /usr/local/bin/entrypoint.sh
