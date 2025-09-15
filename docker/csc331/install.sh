de48a629362ade2091bd350ee023374a55e2e3ff
Linh B Ngo <lngo@wcupa.edu>
2025-09-15 13:12:00 -0400

    Update install.sh

--- docker/csc331/install.sh
+++ docker/csc331/install.sh
@@ -6,10 +6,15 @@ DEBIAN_FRONTEND=noninteractive apt install -y \
     gdb \
     valgrind \
     qemu-system-misc \
-    gcc-riscv64-unknown-elf 
+    gcc-riscv64-unknown-elf \
+    python3-six
 
 apt clean
 rm -rf /var/lib/apt/lists/*
+
+git clone https://github.com/longld/peda.git /opt/peda
+echo "source /opt/peda/peda.py" > /home/student/.gdbinit
+chown student:student /home/student/.gdbinit
 
 # setup code server
 sh /home/student/build/code-server.sh #--prefix=/usr/local/
